13604c18007516a17ee07df1fd53ed91ada18521
steward-fu <g9313716@yuntech.edu.tw>
2016-10-05 22:16:54 +0800

    20161005

---
+++ prebuilt-toolchain/lib/gcc/psp/4.9.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix=/usr/local/pspdev --target=psp --enable-languages=c,c++ --enable-lto --with-newlib --with-gmp-include=/home/steward/Downloads/psptoolchain/build/gcc-4.9.3/build-psp/gmp --with-gmp-lib=/home/steward/Downloads/psptoolchain/build/gcc-4.9.3/build-psp/gmp/.libs --with-mpfr-include=/home/steward/Downloads/psptoolchain/build/gcc-4.9.3/build-psp/../mpfr/src --with-mpfr-lib=/home/steward/Downloads/psptoolchain/build/gcc-4.9.3/build-psp/mpfr/src/.libs --enable-cxx-flags=-G0";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
